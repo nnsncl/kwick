eaa76103a8282e824d5684443126cf86be79ea67
Nunes <nicolas.mgta+git@gmail.com>
2021-01-04 12:31:26 +0100

    Initial GET requestion

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  async function getData() {
+    try {
+      const response = await axios.get('http://greenvelvet.alwaysdata.net/kwick/api/ping',{
+        params: {
+          dataType: 'json'
+        }
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div>
       a
