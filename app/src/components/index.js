7c485244bab0b660ff2ef350cd9dd62d6c082c94
nnsncl <nicolas.mgta@gmail.com>
2021-01-05 00:57:52 +0100

    export components

--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Navigation } from './Header/index';
+export { default as Layout } from './Layout/index';
+export { default as Typography } from './Typography/index';
+
 export * from './AnimatedTitle/index'
