f04a789e6321d8fc76066609ce7b7eb0c78949f4
nnsncl <nicolas.mgta@gmail.com>
2021-01-04 20:36:50 +0100

    send message feature

--- app/src/pages/chat.js
+++ app/src/pages/chat.js
@@ -9,6 +9,7 @@ export default function Chat() {
     const [users, setUsers] = useState([]);
 
     const [messages, setMessages] = useState([]);
+    const [message, setMessage] = useState('');
 
     console.log(auth.localStorageUser);
     console.log(auth);
@@ -42,23 +43,45 @@ export default function Chat() {
     }, [])
 
 
-    
+    const handlePostMessage = () => {
+        async function sendMessage() {
+            try {
+                await axios.get(`${auth.apiURL}/say/${auth.localStorageUser.token}/${auth.localStorageUser.id}/${message}`);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        sendMessage();
+    }
+
 
     return auth.localStorageUser ? (
         <>
+            <h2>Users</h2>
             {users && users.map((user) => (
                 <p key={user} >
                     {user}
                 </p>
             ))}
 
-            {/* {messages && messages.map((message) => (
+            <h2>Messages</h2>
+            {messages && messages.map((message) => (
                 <div key={message.timestamp} >
                     <p>{message.timestamp}</p>
                     <p>{message.user_name}</p>
                     <p>{message.content}</p>
                 </div>
-            ))} */}
+            ))}
+
+            <form onSubmit={handlePostMessage} >
+                <input
+                    placeholder="Send a message"
+                    value={message ? message : ''}
+                    type='text'
+                    onChange={ e => {setMessage(e.target.value)} }
+                />
+                <button type='submit' >Send</button>
+            </form>
 
         </>
     ) : (<Redirect to="/signin" />);
