84ca2cf287e64fc4fbef6cdc0d7e4f8c3cbfcdfd
nnsncl <nicolas.mgta@gmail.com>
2021-01-04 13:17:51 +0100

    add basic router

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { Signup } from './pages';
+
 
 function App() {
 
   return (
-    <p>
-      default
-    </p>
+<Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/signup">Sign up</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
