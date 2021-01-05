32b40ca7e09f5bd7a403e5a0aba6ee1119369ef8
nnsncl <nicolas.mgta@gmail.com>
2021-01-05 02:12:23 +0100

    Edit title UI

--- app/src/components/AnimatedTitle/styles/AnimatedTitle.js
+++ app/src/components/AnimatedTitle/styles/AnimatedTitle.js
@@ -4,12 +4,14 @@ import { motion } from 'framer-motion'
 export const Container = styled(motion.h1)`
   overflow: hidden;
   width: 100%;
-  font-size: 8.6rem;
+  font-size: 7.4rem;
   font-weight: 400;
   letter-spacing: -3.6px;
+  font-weight: bolder;
   transition: font-size ease-in .2s;
   @media(max-width: 1056px) {
-    font-size: 6.8rem;
+    font-size: 3.6rem;
+    letter-spacing: -2.3px;
   }
 `;
 
