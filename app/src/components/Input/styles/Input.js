7a53d314f5dd58d9cafc683d2e8af8b772bded1e
nnsncl <nicolas.mgta@gmail.com>
2021-01-05 02:12:31 +0100

    Edit input states

--- app/src/components/Input/styles/Input.js
+++ app/src/components/Input/styles/Input.js
@@ -4,22 +4,30 @@ export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     margin-bottom: 1.3rem;
-    width: 100%;
 `;
 
 export const Label = styled.label`
     font-size: 1.3rem;
     margin-bottom: 0.9rem;
+    font-weight: bolder;
 `;
 export const Field = styled.input`
     background: white;
     border-radius: 9px;
-    border: none;
+    border: 1px solid white;
     padding: 1.3rem 1.9rem;
     font-size: 1.6rem;
     outline: none;
     transition: all ease-out .2s;
     ::placeholder{
         opacity: 0.3;
+    }
+    :hover, :active, :focus {
+        border-color: #D8D8D8;
+    }
+    :disabled{
+        background: #D8D8D8;
+        border-color: #D8D8D8;
+        cursor: not-allowed;
     }
 `;
