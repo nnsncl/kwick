49ff7908fcf1c154dfa197278df2d475155483c4
nnsncl <nicolas.mgta@gmail.com>
2021-01-08 12:27:29 +0100

    document hooks

--- app/src/hooks/use-auth.js
+++ app/src/hooks/use-auth.js
@@ -4,18 +4,24 @@ import React, { useState, useContext, createContext } from "react";
 import { useLocalStorage } from './use-local-storage';
 import axios from 'axios';
 
-const authContext = createContext();
+// API url from .env
 export const apiURL = process.env.REACT_APP_API_URL;
 
+const authContext = createContext();
+// Provider component that wraps your app and makes auth object ...
+// ... available to any child component that calls useAuth().
 export function ProvideAuth({ children }) {
   const auth = useProvideAuth();
   return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
+// Hook for child components to get the auth object ...
+// ... and re-render when it changes.
 export const useAuth = () => {
   return useContext(authContext);
 };
 
+// Provider hook that creates auth object and handles state
 function useProvideAuth() {
   const [user, setUser] = useState(null);
   const [localStorageUser, setLocalStorageUser] = useLocalStorage('user', user);
@@ -35,20 +41,26 @@ function useProvideAuth() {
           dataType: 'JSON'
         }
       });
+
+      // Set an object in localStorage if the server's repsonse isn't failure
       if (response.data.result.status !== 'failure') {
         setLocalStorageUser({
           username: username,
           token: response.data.result.token,
           id: response.data.result.id
         })
+        // Set the user with the datas stored in localStorageUser
+        // ... set errors to null
         setUser(localStorageUser);
         setSigninError(null);
         setSigninErrorMessage(null);
       } else {
+        // Set errors to true and store the message value
         setSigninError(true);
         setSigninErrorMessage(`${response.data.result.message}`);
       }
     } catch (error) {
+      // If server's repsonse is another error than failure, set errors to true
       setSigninError(true);
     }
   }
@@ -79,23 +91,26 @@ function useProvideAuth() {
   }
 
 
+  // Get messages list from Kwick API
   async function getMessages() {
     try {
-        const response = await axios.get(`${apiURL}/talk/list/${localStorageUser.token}/0`);
-        setMessages(response.data.result.talk);
+      const response = await axios.get(`${apiURL}/talk/list/${localStorageUser.token}/0`);
+      setMessages(response.data.result.talk);
 
     } catch (error) {
-        console.error(error);
-    }
-}
-
-  function signout() {
-    if(localStorageUser) {
-      setLocalStorageUser(null);
+      console.error(error);
     }
   }
 
-  // Return the user object and auth methods
+  function signout() {
+    // remove datas from localStorage
+    if (localStorageUser) {
+      setLocalStorageUser(null);
+    }
+
+  }
+
+  // Return auth methods, localStorageUser datas and get messages mothod
   return {
     apiURL,
     localStorageUser,
