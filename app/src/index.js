1ac5a5b1ffbaacd07088e0a281b1ad3b12cb4703
nnsncl <nicolas.mgta@gmail.com>
2021-01-08 12:45:55 +0100

    document index

--- app/src/index.js
+++ app/src/index.js
@@ -6,6 +6,7 @@ import { GlobalStyles } from './styles/global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* Apply global styles to the app */}
     <GlobalStyles />
     <App />
   </React.StrictMode>,
